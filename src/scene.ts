b1a3835dd29def9813f18645c8a1bcffd25a2ea9
Admiral Potato <admiral@nuclearpixel.com>
2026-09-07 02:30:39 -0600

    Add simple physics implementation

--- src/scene.ts
+++ src/scene.ts
@@ -1,6 +1,6 @@
 import {
 	type Bone,
-	type SkinnedMesh,
+	// type SkinnedMesh,
 	Scene,
 	WebGLRenderer,
 	PerspectiveCamera,
@@ -14,12 +14,20 @@ import {
 	MeshStandardMaterial,
 	PCFShadowMap,
 	Object3D,
+	LineSegments,
+	BufferGeometry,
+	LineBasicMaterial,
+	BufferAttribute,
+	Quaternion,
+	ArrowHelper,
+	SkeletonHelper,
 } from 'three';
 
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader, type GLTF } from 'three/addons/loaders/GLTFLoader.js';
 import { RapierPhysics } from 'three/addons/physics/RapierPhysics.js';
+import type { Collider, RigidBody } from '@dimforge/rapier3d-compat';
 
 const TAU = Math.PI * 2;
 
@@ -28,11 +36,18 @@ const [physics, geepLTF] = await Promise.all([
 	RapierPhysics(),
 	new Promise<GLTF>((resolve) => gltfLoader.load('geep.glb', resolve)),
 ]);
+const { RAPIER, world } = physics;
 
 const params = {
-	geepWiggleSpeed: 100,
+	geepWiggleSpeed: 200,
 	geepWiggleIntensity: 1,
-	geepSpinSpeed: 100,
+	showPhysics: true,
+	reset() {
+		const rigidBody: RigidBody = geepParent.userData.physics.body;
+		rigidBody.setTranslation(new Vector3(), true);
+		rigidBody.setRotation(new Quaternion(), true);
+		rigidBody.resetForces(true);
+	},
 };
 
 const renderer = new WebGLRenderer({ antialias: true, alpha: true });
@@ -44,6 +59,8 @@ const scene = new Scene();
 
 const camera = new PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.set(10, 10, 20);
+// camera.position.set(20, 0, 0); // right side
+// camera.position.set(0, 20, 0); // top side
 
 let width = 0;
 let height = 0;
@@ -94,13 +111,14 @@ const gui = new GUI();
 
 gui.add(params, 'geepWiggleSpeed', 0, 400);
 gui.add(params, 'geepWiggleIntensity', 0, 2);
-gui.add(params, 'geepSpinSpeed', 0, 400);
-
+gui.add(params, 'showPhysics').name('Physics Debug Renderer');
+gui.add(params, 'reset');
 // end init gui
 
-const geepParentMesh = new SphereGeometry(4, 6, 4);
+const geepParentMesh = new SphereGeometry(1, 6, 4);
 const geepParentMaterial = new MeshBasicMaterial({ wireframe: true });
 const geepParent = new Mesh(geepParentMesh, geepParentMaterial);
+geepParentMaterial.visible = false;
 scene.add(geepParent);
 
 const geepScene = geepLTF.scene;
@@ -110,7 +128,9 @@ geepScene.traverse(function (child) {
 	}
 });
 const geepBones = geepScene.getObjectByName('geep_bones') as Object3D;
-const geep = geepScene.getObjectByName('geep_bones') as SkinnedMesh;
+// const geep = geepScene.getObjectByName('geep') as SkinnedMesh;
+const skeletonHelper = new SkeletonHelper(geepBones);
+scene.add(skeletonHelper);
 // const hip = geepScene.getObjectByName('hip') as Bone;
 const legL = geepScene.getObjectByName('hind_1_L') as Bone;
 const legR = geepScene.getObjectByName('hind_1_R') as Bone;
@@ -118,43 +138,141 @@ const armL = geepScene.getObjectByName('leg_1_L') as Bone;
 const armR = geepScene.getObjectByName('leg_1_R') as Bone;
 const spine0 = geepScene.getObjectByName('spine_0') as Bone;
 const spine1 = geepScene.getObjectByName('spine_1') as Bone;
-console.log('geep', geep);
-geepBones.position.y = -4.125;
+geepBones.position.y = -3.75;
 geepParent.userData.physics = { mass: 1, restitution: 0.99 };
 geepParent.add(geepScene);
 
 const floorSize = 100;
-const floorPlane = new BoxGeometry(floorSize, 0.5, floorSize);
+const floorPlane = new BoxGeometry(floorSize, floorSize, floorSize);
 const floorMaterial = new MeshStandardMaterial();
 const floorPlaneMesh = new Mesh(floorPlane, floorMaterial);
 floorPlaneMesh.receiveShadow = true;
-floorPlaneMesh.position.y = -7;
+floorPlaneMesh.position.y = -floorSize / 2 - 7;
 floorPlaneMesh.userData.physics = { mass: 0, restitution: 1 };
 scene.add(floorPlaneMesh);
+physics.addScene(scene);
+// physics.setMeshVelocity(geepParent, new Vector3(0, 5, 0));
+const geepRigidBody: RigidBody = geepParent.userData.physics.body;
+const legRadius = 1;
+const legLength = 1.125;
+const limbRestitution = 0.5;
+const armsShape = RAPIER.ColliderDesc.capsule(legLength, legRadius)
+	.setMass(1000)
+	.setRestitution(limbRestitution);
+const legsShape = RAPIER.ColliderDesc.capsule(legLength, legRadius)
+	.setMass(1000)
+	.setRestitution(limbRestitution);
+const headShape = RAPIER.ColliderDesc.ball(legRadius).setMass(1000).setRestitution(limbRestitution);
+const armsCollider = world.createCollider(armsShape, geepRigidBody);
+const legsCollider = world.createCollider(legsShape, geepRigidBody);
+const headCollider = world.createCollider(headShape, geepRigidBody);
 
+const armsRotation = new Quaternion(1, 0, 0, 0);
+const armsRotationAxis = new Vector3(1, 0, 0);
+const legsRotation = new Quaternion(1, 0, 0, 0);
+const legsOffset = new Vector3(0, -legLength, 0);
+const armsOffset = new Vector3(0, legLength, 0);
+const applyPivotedColliderRotation = (
+	collider: Collider,
+	offset: Vector3,
+	pivot: Vector3,
+	rotation: Quaternion,
+) => {
+	const rotatedPivot = new Vector3().sub(offset).applyQuaternion(rotation).add(pivot);
+	collider.setTranslationWrtParent(rotatedPivot);
+	collider.setRotationWrtParent(rotation);
+};
+const getRelativePosition = (child: Object3D, relativeParent: Object3D): Vector3 => {
+	const v = new Vector3();
+	v.copy(child.position);
+	child.localToWorld(v);
+	relativeParent.worldToLocal(v);
+	return v;
+};
+const armsPivotHelper = new ArrowHelper(new Vector3(1, 0, 0), undefined, 3, 0x00ff00);
+const legsPivotHelper = new ArrowHelper(new Vector3(1, 0, 0), undefined, 3, 0xff0000);
+const headHelper = new ArrowHelper(new Vector3(1, 0, 0), undefined, 3, 0x0000ff);
+(armsPivotHelper.line.material as MeshBasicMaterial).depthTest = false;
+(legsPivotHelper.line.material as MeshBasicMaterial).depthTest = false;
+(headHelper.line.material as MeshBasicMaterial).depthTest = false;
+armR.add(armsPivotHelper);
+legL.add(legsPivotHelper);
+geepBones.getObjectByName('ear_L')!.add(headHelper);
+// const geepCollider = world.createCollider(geepParent.userData.physics.body, geepRigidBody);
+// const geepCollider = world.(geepParent.userData.physics.body, geepRigidBody);
+const wallSize: [number, number, number] = [1, 20, 20];
+const wallSpacing = 2.5;
+const lWallShape = RAPIER.ColliderDesc.cuboid(...wallSize).setTranslation(wallSpacing, 0, 0);
+const rWallShape = RAPIER.ColliderDesc.cuboid(...wallSize).setTranslation(-wallSpacing, 0, 0);
+const wallRigidBody = world.createRigidBody(RAPIER.RigidBodyDesc.fixed());
+world.createCollider(lWallShape, wallRigidBody);
+world.createCollider(rWallShape, wallRigidBody);
+
+const makeRapierDebug = () => {
+	const geometry = new BufferGeometry();
+	const mesh = new LineSegments(
+		geometry,
+		new LineBasicMaterial({ color: 0xffffff, vertexColors: true }),
+	);
+	mesh.frustumCulled = false;
+	const showList: Object3D[] = [mesh, skeletonHelper, armsPivotHelper, legsPivotHelper, headHelper];
+	scene.add(mesh);
+	return () => {
+		if (params.showPhysics) {
+			const { vertices, colors } = physics.world.debugRender();
+			geometry.setAttribute('position', new BufferAttribute(vertices, 3));
+			geometry.setAttribute('color', new BufferAttribute(colors, 4));
+			showList.forEach((o) => (o.visible = true));
+			mesh.visible = true;
+		} else {
+			showList.forEach((o) => (o.visible = false));
+		}
+	};
+};
+const updateRapierDebug = makeRapierDebug();
+
+const flattenX = new Vector3(0, 1, 1);
 let lastTime = performance.now();
 function animate() {
 	resize();
 	const now = performance.now() / 1000;
 	// const delta = now - lastTime;
 	lastTime = now;
-	geepBones.rotation.y = TAU * params.geepSpinSpeed * 0.005 * now;
+	// geepBones.rotation.y = TAU * params.geepSpinSpeed * 0.005 * now;
 	const phase = params.geepWiggleSpeed * 0.1 * now;
 	// hip.rotation.x = TAU * (1.55 - Math.cos(phase) * 0.0625);
-	const limbs = TAU * Math.cos(phase) * 0.0625 * params.geepWiggleIntensity;
+	const arms = TAU * Math.cos(phase) * 0.0625 * params.geepWiggleIntensity;
 	const legs = TAU * Math.cos(phase) * 0.125 * params.geepWiggleIntensity;
 	const spine = TAU * Math.cos(phase) * 0.025 * params.geepWiggleIntensity;
 	legL.rotation.z = -legs + TAU * -0.625;
 	legR.rotation.z = legs + TAU * 0.625;
-	armL.rotation.z = limbs + TAU * 0.125;
-	armR.rotation.z = -limbs + TAU * -0.125;
+	armL.rotation.z = arms + TAU * 0.125;
+	armR.rotation.z = -arms + TAU * -0.125;
 	spine0.rotation.y = -spine;
 	spine1.rotation.x = -spine;
+	armsRotation.setFromAxisAngle(armsRotationAxis, TAU * -0.15 - arms - spine * 2);
+	legsRotation.setFromAxisAngle(armsRotationAxis, TAU * 0.625 + legs);
+	armsPivotHelper.position.x = 0;
+	legsPivotHelper.position.x = 0;
+	applyPivotedColliderRotation(
+		armsCollider,
+		armsOffset,
+		getRelativePosition(armsPivotHelper, geepParent).multiply(flattenX),
+		armsRotation,
+	);
+	applyPivotedColliderRotation(
+		legsCollider,
+		legsOffset,
+		getRelativePosition(legsPivotHelper, geepParent).multiply(flattenX),
+		legsRotation,
+	);
+	headCollider.setTranslationWrtParent(
+		getRelativePosition(headHelper, geepParent).multiply(flattenX),
+	);
+	updateRapierDebug();
 	renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
-physics.addScene(scene);
-physics.setMeshVelocity(geepParent, new Vector3(0, 5, 0));
 
 export const cleanup = () => {
 	gui.destroy();
@@ -167,10 +285,16 @@ Object.assign(window, {
 	renderer,
 	camera,
 	geepParent,
+	geepRigidBody,
+	armsShape,
+	legsShape,
+	armsRotation,
+	legsRotation,
+	armsCollider,
+	legsCollider,
 	pointLight,
 	floorPlane,
 	geepScene,
-	geep,
-	hip: legL,
 	physics,
+	RAPIER,
 });
